8e7a776b67e2b7933db277aadb281e34e149935e
Dovi KAPLAN <dkaplan@e5r4p6.wethinkcode.co.za>
2018-01-14 18:12:54 +0200

    add day04

--- day03/ex07/strrev.c
+++ day03/ex07/strrev.c
@@ -1,23 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   strrev.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dkaplan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/12 09:13:45 by dkaplan           #+#    #+#             */
-/*   Updated: 2018/01/12 09:13:49 by dkaplan          ###   ########.fr       */
+/*   Created: 2018/01/12 13:32:33 by dkaplan           #+#    #+#             */
+/*   Updated: 2018/01/12 14:14:15 by dkaplan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+char	*ft_strrev(char *str)
 {
-	int	counter;
-	counter = 0;
-	while (*str)
-    {
-		str++;
-		counter++;
+	int	i;
+	int	l;
+	char temp;
+
+	i = 0;
+	l = -1;
+	while (str[i])
+		i++;
+	while (l > i)
+	{
+		temp = str[i];
+		str[i] = str[l];
+		str[l] = temp;
+		l--;
+		i++;
 	}
-	return (counter);
+	return (str);
 }
